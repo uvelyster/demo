6cd02d2bafd86dbf59d7e934a6f468570189da1c
Student User <student@workstation.lab.example.com>
2021-09-07 21:54:35 -0400

    top job

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "Hello world!!\nit works\n good job\n"
+    return "top job" 
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
