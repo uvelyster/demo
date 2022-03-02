11fd9bd22822a465fdb23f80a3fcb3cacbd236ea
SeungUk Oh <uvely@naver.com>
2022-03-02 00:45:09 -0500

    test for trigger

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "good job. it works\n" 
+    return "top job. nice work\n"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
