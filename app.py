cdee4824bda238f0bdbd9c26f805f2a5e131fe36
uvelyster <48861250+uvelyster@users.noreply.github.com>
2022-09-20 14:21:36 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "great job. it works\n"
+    return "this is demo test. first test.\n"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
