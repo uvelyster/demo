5eddef380a3bd2c0adab09dc76b9b759b5fb8ed8
uvelyster <48861250+uvelyster@users.noreply.github.com>
2023-10-27 17:30:47 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "it works hello world!!\n"
+    return "it works hello world!! goodjob!!\n"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
