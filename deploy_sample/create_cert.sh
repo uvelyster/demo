d143bf69a31e83ab0379c30d0d57b841bab59745
uvelyster <48861250+uvelyster@users.noreply.github.com>
2024-05-08 20:19:51 +0900

    Update create_cert.sh

--- deploy_sample/create_cert.sh
+++ deploy_sample/create_cert.sh
@@ -2,16 +2,16 @@
 
 mkdir /auth 
 
-# openssl req -newkey rsa:4096 -nodes -sha256 -x509 \
+openssl req -newkey rsa:4096 -nodes -sha256 -x509 \
+ -days 365 -keyout /auth/myregistry.com.key -out /auth/myregistry.com.crt \
+ -subj '/CN=myregistry.com' \
+ -addext "subjectAltName = DNS:myregistry.com"
+
+# docker run --rm -v /auth:/auth alpine/openssl \
+# req -newkey rsa:4096 -nodes -sha256 -x509 \
 # -days 365 -keyout /auth/myregistry.com.key -out /auth/myregistry.com.crt \
 # -subj '/CN=myregistry.com' \
 # -addext "subjectAltName = DNS:myregistry.com"
-
-docker run --rm -v /auth:/auth alpine/openssl \
-req -newkey rsa:4096 -nodes -sha256 -x509 \
--days 365 -keyout /auth/myregistry.com.key -out /auth/myregistry.com.crt \
--subj '/CN=myregistry.com' \
--addext "subjectAltName = DNS:myregistry.com"
 
 mkdir -p /etc/docker/certs.d/myregistry.com
 cp /auth/myregistry.com.crt /etc/docker/certs.d/myregistry.com/ca.crt
